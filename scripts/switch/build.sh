cb7e91442de30837aa39c5f76124b66f6bb61c8a
xlanor <contact@jingk.ai>
2024-11-05 16:54:51 +0000

    openssl was fucked
    Signed-off-by: xlanor <contact@jingk.ai>

--- scripts/switch/build.sh
+++ scripts/switch/build.sh
@@ -31,9 +31,11 @@ build_chiaki (){
 			-DCHIAKI_ENABLE_STEAMDECK_NATIVE=OFF\
 			-DCHIAKI_ENABLE_STEAM_SHORTCUT=OFF \
 			-DCMAKE_FIND_DEBUG_MODE=OFF \
-			-DOPENSSL_INCLUDE_DIR="/usr/include/openssl" \ 
+			-DJSON_C_LIBRARY_DIRS=/usr/lib \
+			-DOPENSSL_ROOT_DIR="/usr/include/openssl" \
+			-DOPENSSL_INCLUDE_DIR="/usr/include/openssl" \
 			-DOPENSSL_SSL_LIBRARY="/usr/lib/libssl.so.3" \
-			-DOPENSSL_CRYPTO_LIBRARY="/usr/lib/libcrypto.so.3" \
+			-DOPENSSL_CRYPTO_LIBRARY="/usr/lib/libcrypto.so.3"
 			# -DCMAKE_VERBOSE_MAKEFILE:BOOL=ON \
 			# -DCMAKE_FIND_DEBUG_MODE=ON
 
