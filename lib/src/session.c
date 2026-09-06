32666bbee24c0d3ee70232007ba81f2f34bdd532
xlanor <contact@jingk.ai>
2026-09-06 21:31:52 +0100

    add 1440p and 2160p video resolution presets

--- lib/src/session.c
+++ lib/src/session.c
@@ -114,6 +114,16 @@ CHIAKI_EXPORT void chiaki_connect_video_profile_preset(ChiakiConnectVideoProfile
 			profile->height = 1080;
 			profile->bitrate = 15000;
 			break;
+		case CHIAKI_VIDEO_RESOLUTION_PRESET_1440p:
+			profile->width = 2560;
+			profile->height = 1440;
+			profile->bitrate = 30000;
+			break;
+		case CHIAKI_VIDEO_RESOLUTION_PRESET_2160p:
+			profile->width = 3840;
+			profile->height = 2160;
+			profile->bitrate = 50000;
+			break;
 		default:
 			profile->width = 0;
 			profile->height = 0;
